54bf3380ebbeb1d42a498132133943d488d83d5d
M.Rizwan <rizwan02091986@gmail.com>
2024-05-11 20:25:04 +0500

    11-05-2024

--- user panel/user panel/index.php
+++ user panel/user panel/index.php
@@ -47,81 +47,26 @@
 
 							<!-- Products -->
 							<section class="tiles">
+                                <?php
+                                $sql = "select * from category order by id asc limit 3";
+                                $result = mysqli_query($conn,$sql);
+                                while($rows = mysqli_fetch_assoc($result)){
+                                ?>
 								<article class="style1">
 									<span class="image">
-										<img src="images/product-1-720x480.jpg" alt="" />
+                                    <?php
+										echo "<img src=\"../../admin/images/category/{$rows['CategoryImage']} \" height=300px; width=300px>"
+                                        ?>
 									</span>
 									<a href="product-details.html">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
+										<h2><?php echo $rows['CategoryName'] ?></h2>
 										
 										<p><del>$19.00</del> <strong>$19.00</strong></p>
 
 										<p>Vestibulum id est eu felis vulputate hendrerit uspendisse dapibus turpis in </p>
 									</a>
 								</article>
-								<article class="style2">
-									<span class="image">
-										<img src="images/product-2-720x480.jpg" alt="" />
-									</span>
-									<a href="product-details.html">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$19.00</del> <strong>$19.00</strong></p>
-
-										<p>Vestibulum id est eu felis vulputate hendrerit uspendisse dapibus turpis in </p>
-									</a>
-								</article>
-								<article class="style3">
-									<span class="image">
-										<img src="images/product-3-720x480.jpg" alt="" />
-									</span>
-									<a href="product-details.html">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$19.00</del> <strong>$19.00</strong></p>
-
-										<p>Vestibulum id est eu felis vulputate hendrerit uspendisse dapibus turpis in </p>
-									</a>
-								</article>
-
-								<article class="style4">
-									<span class="image">
-										<img src="images/product-4-720x480.jpg" alt="" />
-									</span>
-									<a href="product-details.html">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$19.00</del> <strong>$19.00</strong></p>
-
-										<p>Vestibulum id est eu felis vulputate hendrerit uspendisse dapibus turpis in </p>
-									</a>
-								</article>
-
-								<article class="style5">
-									<span class="image">
-										<img src="images/product-5-720x480.jpg" alt="" />
-									</span>
-									<a href="product-details.html">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$19.00</del> <strong>$19.00</strong></p>
-
-										<p>Vestibulum id est eu felis vulputate hendrerit uspendisse dapibus turpis in </p>
-									</a>
-								</article>
-
-								<article class="style6">
-									<span class="image">
-										<img src="images/product-6-720x480.jpg" alt="" />
-									</span>
-									<a href="product-details.html">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$19.00</del> <strong>$19.00</strong></p>
-
-										<p>Vestibulum id est eu felis vulputate hendrerit uspendisse dapibus turpis in </p>
-									</a>
-								</article>
+                                <?php } ?>
 							</section>
 
 							<p class="text-center"><a href="products.html">More Books &nbsp;<i class="fa fa-long-arrow-right"></i></a></p>
